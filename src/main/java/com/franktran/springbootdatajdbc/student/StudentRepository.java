48afd58c369c94f32856ff309ac472622ded016b
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-13 18:24:18 +0700

    Initial commit

---
+++ src/main/java/com/franktran/springbootdatajdbc/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.franktran.springbootdatajdbc.student;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+}
